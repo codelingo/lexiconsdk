926c31e969810fa5c40ef7bd30d44377ae710cec
Junyu <zhujunyu168@hotmail.com>
2017-09-13 17:47:45 +1200

    add setup scripts and readme

---
+++ examples/lexicons/codelingo/ast/csharp/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+sudo cp -R testdata/ /testdata/
+src/build.sh
+sudo cp -R src/ /server/
+sudo cp server-config.yaml /server/
+sudo mkdir -p /setup/
+sudo cp setup.json /setup/
+
+user="$(whoami)"
+sudo chown -R $user:$user /testdata /server /setup
+
--- examples/lexicons/codelingo/ast/csharp/src/build.sh
+++ examples/lexicons/codelingo/ast/csharp/src/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-dotnet restore "$GOPATH/src/github.com/codelingo/lexicon/codelingo/ast/csharp/src/lexicon.csproj"
-dotnet publish -c Release -o lexicon "$GOPATH/src/github.com/codelingo/lexicon/codelingo/ast/csharp/src/lexicon.csproj"
+echo `pwd`
+dotnet restore "./src/lexicon.csproj"
+dotnet publish -c Release -o lexicon "./src/lexicon.csproj"
